e7f90ca847a4bdd7bc4042598752b1d758cd8618
Vinay Kumar <vkr7.info@gmail.com>
2026-02-16 15:34:40 +0530

    02CustomReact

---
+++ 02CustomReact/react.js
@@ -0,0 +1,21 @@
+function customRender(reactElement, container) {
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    for (const prop in reactElement.props) {
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://vinaydev.in",
+        target: '_blank'
+    },
+    children: 'Click me to go to my website'
+}
+
+const mainContainer = document.getElementById('root');
+customRender(reactElement, mainContainer)
