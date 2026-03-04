510e4a51ee4b13f93d0e00eb3d83be95bb83c336
Vinay Kumar <vkr7.info@gmail.com>
2026-03-04 23:40:39 +0530

    Input-Card-Select-Component-done

--- MegaBlog/src/components/Input.jsx
+++ MegaBlog/src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React, { useId } from "react";
 
-const Input = React.forwardRef(function Input(
+const Input = React.forwardRef(function Input( //forwardRef is used to pass the ref to the input where it is used so that state can be accessed
   { label, type = "text", className = "", ...props },
   ref,
 ) {
